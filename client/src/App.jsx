6391571cdc85c6bf88a5bcf368e0658cd952b188
Christopher Rivera <riveramercado.christopher@gmail.com>
2023-01-24 20:53:12 -0400

    Added React front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <h1 className='container mt-5'>Favorite Authors</h1>
+  )
+}
+
+export default App
